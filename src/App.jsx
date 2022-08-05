e4ec2368a99aacc82b5e7547d717b9286148f0cc
BielPimentaDev <79238035+BielPimentaDev@users.noreply.github.com>
2022-08-05 20:34:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-
 import Home from './Home'
 import './App.css'
-import Header from './common/Header'
-import Homepage from './Home/Homepage'
-import About from './Home/About'
 
 function App() {
  
